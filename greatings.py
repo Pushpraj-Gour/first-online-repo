7e642ec37b635649063952269f2fef0f8e65706a
Pushpraj <rajrjpushp@gmail.com>
2023-07-09 15:16:30 +0530

    adding summation file

--- greatings.py
+++ greatings.py
@@ -1,1 +1,3 @@
 print("Hello!!! Today we will initialize our first project and track everything with github.")
+
+print("This is just to check the changes")
---
+++ sum.py
@@ -0,0 +1,3 @@
+a = 3
+b = 2
+print("Sum of 2 numbers is ",a+b)
